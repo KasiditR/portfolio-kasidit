64fbc2a9ff02ddc4d4374e2f048b8ebf4303abce
boss <kasidit.ruangmunjit@gmail.com>
2023-11-21 20:03:57 +0700

    added link GOR

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -29,13 +29,13 @@ const Portfolio = () => {
     },
     {
       title: "Glory of Ragnarok",
-      description: "​Innovation player need to find all the key value of JD Central to pass the level. (Web Game)",
+      description: "Idle game Glory Of Ragnarok, the war between the Spirit Sealers Association and the demons who are ready to destroy the 5 kingdoms.“Star Stone” is a magical power refined from the spirits of earth and heaven, but! Instead, it was destroyed by the demon queen named Merida. The power of the Star Stone reverted to destroying the world, and the five kingdoms were soon about to collapse.",
       imgUrl: project4,
-      websiteUrl: "https://innovation01-c8042.web.app/",
+      websiteUrl: "https://play.google.com/store/apps/details?id=com.glory.ragnarok",
       buttonOne: "Google",
-      buttonOneUrl: "https://www.google.com",
+      buttonOneUrl: "https://play.google.com/store/apps/details?id=com.glory.ragnarok",
       buttonTwo: "iOS",
-      buttonTwoUrl: "https://www.apple.com",
+      buttonTwoUrl: "https://apps.apple.com/us/app/glory-of-ragnarok/id6449695630",
     },
   ];
   return (
